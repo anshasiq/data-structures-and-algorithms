6ac457b41ca5dafb3baa8800d73b20c7b940d2e6
anshasiq <ahmadanshasi123@hotmail.com>
2023-11-17 18:57:00 +0000

    last one for cc33

--- hashmap/app/src/test/java/hashmap/LiftJoinLab33.java
+++ hashmap/app/src/test/java/hashmap/LiftJoinLab33.java
@@ -2,11 +2,14 @@ package hashmap;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static hashmap.App.left_join;
 import static org.junit.jupiter.api.Assertions.*;
 public class LiftJoinLab33 {
     @Test
-    public void test_repeated_word() {
+    public void test_lift_Join() {
         App app = new App();
         Hashtable<String, String>  a = new Hashtable<>();  Hashtable<String, String>  b = new Hashtable<>();
         a.set("diligent","employed");
@@ -19,7 +22,13 @@ public class LiftJoinLab33 {
         b.set("flow","jam");
         a.set("wrath","anger");
         b.set("wrath","delight");
-
+        ArrayList<ArrayList<String>> twoDArrayList = new ArrayList<>(Arrays.asList(
+                new ArrayList<>(Arrays.asList("guide", "usher", "follow")),
+                new ArrayList<>(Arrays.asList("outfit", "garb", null)),
+                new ArrayList<>(Arrays.asList("diligent", "employed", "idle")),
+                new ArrayList<>(Arrays.asList("fond", "enamored", "averse")),
+                new ArrayList<>(Arrays.asList("wrath", "anger", "delight"))
+        ));
 //        System.out.println();
-        assertEquals("[guide, usher, follow], [outfit, garb, null], [diligent, employed, idle], [fond, enamored, averse], [wrath, anger, delight]]",left_join(a,b));
+        assertEquals(twoDArrayList,left_join(a,b));
     }}
