85e928c64313ca05cedd39d9e12d5ace23f3ee76
anshasiq <ahmadanshasi123@hotmail.com>
2023-11-19 17:32:27 +0000

    lastone

---
+++ graph/app/src/main/java/graph/Graph.java
@@ -0,0 +1,71 @@
+package graph;
+
+import java.util.*;
+
+public class Graph {
+    private Map<Vertex, LinkedList<Edge>> adjacencyList;
+
+    public Graph() {
+        this.adjacencyList = new HashMap<>();
+    }
+
+    public Vertex addVertex(String value) {
+        Vertex vertex = new Vertex(value);
+        adjacencyList.put(vertex, new LinkedList<>());
+        return vertex;
+    }
+
+    // Overloaded addEdge method without weight parameter
+
+    public void addEdge(Vertex vertex1, Vertex vertex2) {
+        if (!adjacencyList.containsKey(vertex1) || !adjacencyList.containsKey(vertex2)) {
+            throw new IllegalArgumentException("Both vertices should already be in the graph");
+        }
+
+        adjacencyList.get(vertex1).add(new Edge(vertex2));
+        adjacencyList.get(vertex2).add(new Edge(vertex1));
+    }
+
+
+//
+    public void addEdge(Vertex vertex1, Vertex vertex2, int weight) {
+        if (!adjacencyList.containsKey(vertex1) || !adjacencyList.containsKey(vertex2)) {
+            throw new IllegalArgumentException("Both vertices should already be in the graph");
+        }
+
+        adjacencyList.get(vertex1).add(new Edge(vertex2, weight));
+        adjacencyList.get(vertex2).add(new Edge(vertex1, weight));
+    }
+
+    public Collection<Vertex> getVertices() {
+        return adjacencyList.keySet();
+    }
+
+    public Collection<Edge> getNeighbors(Vertex vertex) {
+        if (!adjacencyList.containsKey(vertex)) {
+            return Collections.emptyList();
+        }
+
+        return adjacencyList.get(vertex);
+    }
+
+    public int size() {
+        return adjacencyList.size();
+    }
+
+    private static class Edge {
+        Vertex vertex;
+        int weight;
+
+        public Edge(Vertex vertex, int weight) {
+            this.vertex = vertex;
+            this.weight = weight;
+        }
+        public Edge(Vertex vertex) {
+            this(vertex, 0);
+        }
+        @Override
+        public String toString() {
+            return "Edge to " + vertex + " with weight " + weight;
+        }
+    }}
